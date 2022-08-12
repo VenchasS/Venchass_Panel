a08f5a34b57c99573b68acc05ccf34698e9d51e8
Venchass <mail.ru>
2022-08-13 00:20:54 +0500

    Добавьте файлы проекта.

---
+++ WPF Vench Launcher/MainWindow.xaml.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Linq;
+
+namespace WPF_Vench_Launcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        public CategoryHighlightStyleSelector CategoryHighlightStyleSelector;
+
+        private bool StartUpParamsChanged = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeFolder();
+            InitConfig();
+            var timer = InitTimer();
+            CustomTitle.MouseLeftButtonDown += new MouseButtonEventHandler(MoveWindow);
+            LogoLabel.MouseLeftButtonDown += new MouseButtonEventHandler(MoveWindow);
+            this.MouseLeftButtonDown += new MouseButtonEventHandler(window_MouseDown);
+        }
+        private void window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            AccountLogin.Focus();
+            Keyboard.ClearFocus();
+        }
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void HideButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+        
+
+        void MoveWindow(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private System.Windows.Threading.DispatcherTimer InitTimer()
+        {
+            System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Tick += new EventHandler(timerTick);
+            timer.Interval = new TimeSpan(0, 0, 2);
+            timer.Start();
+            return timer;
+        }
+
+        private void timerTick(object sender, EventArgs e)
+        {
+            //update accounts info
+            AccountsTotal.Text = accountsList.Items.Count.ToString();
+            AccountsStarted.Text = AccountManager.GetAccountsBase().Where(x => x.Status == 2).Count().ToString();
+            Thread th = new Thread(AccountManager.UpdateAccountsChildrens);
+            Thread th2 = new Thread(AccountManager.SdaCheck);
+            th.Start();
+            th2.Start();
+            //AccountManager.UpdateAccountsChildrens();
+            //AccountManager.SdaCheck();
+            
+            UpdateTable();
+        }
+        List<AccountsGroup> accountGroupsSource = new List<AccountsGroup>() { new AccountsGroup(AccountManager.GetAccountsBase(), "all accs") };
+        public void InitConfig()
+        {
+            Config.LoadAccountsData();
+            accountsList.ItemsSource = AccountManager.GetAccountsBase();
+            var cofig = Config.LoadConfig();
+            accountGroupsSource = cofig.Groups;
+            startupParams.Text = cofig.StartParams;
+            AccountGroups.ItemsSource = accountGroupsSource;
+        }
+
+        private void CheckFile(string name)
+        {
+            var currentDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/VenchassPanel";
+            if (!File.Exists(currentDirectory + name))
+                File.Create(currentDirectory + name);
+        }
+
+        void InitializeFolder()
+        {
+            var currentDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"/VenchassPanel";
+            Config.DirectoryPath = currentDirectory;
+            var dir = new DirectoryInfo(currentDirectory);
+            if (!dir.Exists)
+                dir.Create();
+
+            CheckFile(@"/Accounts.cfg");
+            CheckFile(@"/config.cfg");
+        }
+
+        public void UpdateTable()
+        {
+            accountsList.ItemsSource = AccountManager.GetAccountsBase();
+            StyleSelector selector = accountsList.ItemContainerStyleSelector;
+            accountsList.ItemContainerStyleSelector = null;
+            accountsList.ItemContainerStyleSelector = selector;
+        }
+
+        public void ClearSelected()
+        {
+            accountsList.UnselectAll();
+        }
+
+        private void ButtonAddClick(object sender, RoutedEventArgs e)
+        {
+            var account = new Account(AccountLogin.Text, AccountPassword.Text);
+            AccountManager.AddAccount(account);
+            Config.SaveAccountsDataAsync();
+            UpdateTable();
+        }
+
+
+        private void ButtonStartClick(object sender, RoutedEventArgs e)
+        {
+            var selectedAccounts = accountsList.SelectedItems;
+            foreach (var item in selectedAccounts)
+            {
+                AccountManager.StartAccount((Account)item, startupParams.Text);
+            }
+            Config.SaveAccountsDataAsync();
+            ClearSelected();
+            UpdateTable();
+        }
+
+        private void OnTestButtonclick(object sender, RoutedEventArgs e)
+        {
+            //AccountManager.UpdateAccountsChildrens();
+            //AccountManager.GetWindowHandles();
+            var selected = (AccountsGroup)AccountGroups.SelectedItem;
+            foreach (var acc in selected.InGroupAccounts)
+            {
+                for (int i = 0; i < accountsList.Items.Count; i++)
+                {
+                    if( ((Account)accountsList.Items[i]).Login == acc.Login)
+                        accountsList.SelectedItems.Add(accountsList.Items[i]);
+                }
+            }
+            UpdateTable();
+        }
+
+        private void ButtonStopClick(object sender, RoutedEventArgs e)
+        {
+            var selectedAccounts = accountsList.SelectedItems;
+            foreach (var item in selectedAccounts)
+            {
+                AccountManager.StopAccount((Account)item);
+            }
+            ClearSelected();
+            UpdateTable();
+        }
+
+        private void OnTextBoxStartupParamsChanged(object sender, RoutedEventArgs e)
+        {
+            StartUpParamsChanged = true;
+        }
+
+        private void OnTextBoxStartupParamsLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (StartUpParamsChanged)
+            {
+                Config.SaveStartUpParams(startupParams.Text);
+            }
+            StartUpParamsChanged = false;
+            //save startupParams;
+        }
+
+        private void AccountGroups_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AccountGroups.SelectedItem == null)
+                return;
+            ClearSelected();
+            var selected = (AccountsGroup)AccountGroups.SelectedItem;
+            foreach (var acc in selected.InGroupAccounts)
+            {
+                for (int i = 0; i < accountsList.Items.Count; i++)
+                {
+                    if (((Account)accountsList.Items[i]).Login == acc.Login)
+                        accountsList.SelectedItems.Add(accountsList.Items[i]);
+                }
+            }
+            AccountGroups.UnselectAll();
+            //UpdateTable();
+        }
+
+        private void MakeGroupButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedAccs = accountsList.SelectedItems;
+            var groupAccounts = new List<Account>();
+            foreach (var acc in selectedAccs)
+            {
+                groupAccounts.Add((Account)acc);
+            }
+            var group = new AccountsGroup(groupAccounts, "new groupe");
+            accountGroupsSource.Add(group);
+            var newG = new List<AccountsGroup>(accountGroupsSource);
+            AccountGroups.ItemsSource = newG;
+            Config.SaveGroupsParams(accountGroupsSource);
+        }
+
+        private void DeleteGroup(object sender, RoutedEventArgs e)
+        {
+            var s = sender as Button;
+            
+            var group = s.DataContext;
+            accountGroupsSource.Remove((AccountsGroup)group);
+            var newG = new List<AccountsGroup>(accountGroupsSource);
+            AccountGroups.ItemsSource = newG;
+            Config.SaveGroupsParams(accountGroupsSource);
+        }
+
+        private void GroupNameLostFocus(object sender, RoutedEventArgs e)
+        {
+            Config.SaveGroupsParams(accountGroupsSource);
+        }
+
+    }
+
+    public class CategoryHighlightStyleSelector : StyleSelector
+    {
+        public Style IsStartedStyle { get; set; }
+        public Style IsntStartedStyle { get; set; }
+
+        public Style IsStartedCSGO { get; set; }
+
+        public override Style SelectStyle(object item, DependencyObject container)
+        {
+            Account car = (Account)item;
+
+            switch (car.Status)
+            {
+                case 0:
+                    return IsntStartedStyle;
+                case 1:
+                    return IsStartedStyle;
+                case 2:
+                    return IsStartedCSGO;
+                default:
+                    return null;
+            }
+        }
+    }
+
+    public class AccountsGroup
+    {
+        public List<Account> InGroupAccounts = new List<Account>();
+
+        public string GroupName { get; set; }
+
+        public List<string> AccountLogins { get { return InGroupAccounts.Select(x => x.Login).ToList(); } set {
+               List<string> logins = AccountManager.GetAccountsBase().Select(x => x.Login).ToList();
+            foreach (var item in value)
+                {
+                    if(logins.Contains(item))
+                    {
+                        foreach(var acc in AccountManager.GetAccountsBase())
+                        {
+                            if (acc.Login == item)
+                            {
+                                InGroupAccounts.Add(acc);
+                                break;
+                            }
+
+                        }
+                    }
+                }
+            } }
+
+        public AccountsGroup()
+        {
+
+        }
+
+        public AccountsGroup(List<Account> inGroupAccounts, string groupName)
+        {
+            this.GroupName = groupName;
+            this.InGroupAccounts = inGroupAccounts;
+        }
+    }
+}
---
+++ WPF Vench Launcher/Sources/Accountmanager.cs
@@ -0,0 +1,457 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+using System.Text.Json;
+using System.Windows;
+using System.Runtime.InteropServices;
+using System.Text;
+using SteamAuth;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace WPF_Vench_Launcher
+{
+    
+    public class AccountManager
+    {
+        delegate bool EnumThreadDelegate(IntPtr hWnd, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern bool EnumThreadWindows(int dwThreadId, EnumThreadDelegate lpfn, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern int GetWindowTextLength(IntPtr hWnd);
+
+        static Dictionary<Account, Process> StartedAccountsDict = new Dictionary<Account, Process>();
+
+        static string steamPath = @"C:\Program Files (x86)\Steam";
+
+        static List<Account> AccountsBase = new List<Account>();
+        public static void StartAccount(Account account, string startParams = "")
+        {
+            lock (account)
+            {
+                string path = steamPath + @"\steam.exe";
+                ProcessStartInfo processStartInfo = new ProcessStartInfo()
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    WorkingDirectory = steamPath,
+                    FileName = path,
+                    Arguments = string.Format(" -login {0} {1}  -applaunch 730  {2} ", account.Login, account.Password, startParams)
+                };
+                Process process = new Process()
+                {
+                    StartInfo = processStartInfo
+                };
+                process.Start();
+                account.Status = 1;
+                account.PID = process.Id;
+                SaveAccountData(account, process); //save started acc
+            }
+        }
+
+        private static Process GetProcessByAccount(Account acc)
+        {
+            return StartedAccountsDict[acc];
+        }
+
+        public static void StopAccount(Account acc)
+        {
+            
+            if (acc.Status == 2)
+            {
+                try
+                {
+                    var game = GetGameProcess(acc);
+                    game.Kill();
+                }
+                catch
+                {
+
+                }
+            }
+            lock (StartedAccountsDict)
+            {
+                if (!StartedAccountsDict.ContainsKey(acc))
+                    return;
+                var proc = StartedAccountsDict[acc];
+                proc.Kill();
+                StartedAccountsDict.Remove(acc);
+                acc.Status = 0;
+            }
+        }
+
+        static void SaveAccountData(Account acc, Process proc)
+        {
+            lock (StartedAccountsDict)
+            {
+                StartedAccountsDict.Add(acc, proc);
+            }
+        }
+
+        public static List<Account> GetAccountsBase()
+        {
+            return AccountsBase;
+        }
+
+        public static void AddAccount(Account acc)
+        {
+            AccountsBase.Add(acc);
+            if (acc.PID != 0)
+            {
+                try
+                {
+                    var proc = Process.GetProcessById(acc.PID);
+                    SaveAccountData(acc, proc);
+                }
+                catch
+                {
+                    acc.Status = 0;
+                    acc.PID = 0;
+                }
+            }
+        }
+
+        public static List<Process> GetChildrens(Account account)
+        {
+            lock (StartedAccountsDict)
+            {
+                var children = new List<Process>();
+                if (StartedAccountsDict.ContainsKey(account))
+                {
+                    var process = StartedAccountsDict[account];
+                    var mos = new ManagementObjectSearcher(String.Format($"Select * From Win32_Process Where ParentProcessID={process.Id}"));
+
+                    foreach (ManagementObject mo in mos.Get())
+                    {
+                        try
+                        {
+                            children.Add(Process.GetProcessById(Convert.ToInt32(mo["ProcessID"])));
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+
+                }
+                return children;
+            }
+        }
+
+        public static Process GetGameProcess(Account acc)
+        {
+            var childrens = GetChildrens(acc);
+            foreach (var process in childrens)
+            {
+                if (process.ProcessName == "csgo")
+                {
+                    return process;
+                }
+            }
+            throw new Exception("процесс не найден");
+        }
+
+        public static void UpdateAccountsChildrens()
+        {
+            lock (StartedAccountsDict)
+            {
+                foreach (var pair in StartedAccountsDict)
+                {
+                    if (pair.Key.Status == 0)
+                    {
+                        return;
+                    }
+                    var childrens = GetChildrens(pair.Key);
+                    pair.Key.Status = 1;
+                    foreach (var item in childrens)
+                    {
+                        if (item.ProcessName == "csgo")
+                        {
+                            pair.Key.Status = 2;
+                        }
+                    }
+                }
+            }
+        }
+        
+        private static string GetWindowNameByHwnd(IntPtr hwnd)
+        {
+            int len = GetWindowTextLength(hwnd) + 1;
+            var buff = new StringBuilder(len);
+            if (GetWindowText(hwnd, buff, len) > 0)
+            {
+                return buff.ToString();
+            }
+            return "";
+        }
+
+        public static IEnumerable<IntPtr> GetWindowHandles(Account acc)
+        {
+            Process proc = GetProcessByAccount(acc);
+            var handles = new List<IntPtr>();
+            var names = new List<string>();
+            foreach (ProcessThread thread in proc.Threads)
+                EnumThreadWindows(thread.Id,
+                    (hWnd, lParam) => { handles.Add(hWnd); return true; }, IntPtr.Zero);
+            foreach (var item in handles)
+            {
+                int len = GetWindowTextLength(item) + 1;
+                var buff = new StringBuilder(len);
+                if (GetWindowText(item, buff, len) > 0)
+                {
+                    names.Add(buff.ToString());
+                }
+            }
+            return handles;
+        }
+
+        public static void SdaCheck()
+        {
+            lock (AccountsBase)
+            {
+                foreach (var acc in AccountsBase)
+                {
+                    if (acc.Status == 0)
+                    {
+                        continue;
+                    }
+                    var windows = GetWindowHandles(acc);
+                    foreach (var hwnd in windows)
+                    {
+                        if (GetWindowNameByHwnd(hwnd) == "Steam Guard - Computer Authorization Required")
+                        {
+                            if (SteamGuard.HasGuard(acc.Login.ToLower()))
+                            {
+                                SendText(SteamGuard.GetGuard(acc.Login.ToLower()), hwnd);
+                                SendText("ENTER", hwnd);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern bool PostMessage(IntPtr hWnd, int Msg, char wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private static void SendText(string message , IntPtr hwnd)
+        {
+            const int WM_KEYDOWN = 0x100;
+            const int WM_KEYUP = 0x101;
+            int VK_RETURN = 13;
+            int WM_CHAR = 0x0102;
+            if (message == "ENTER")
+            {
+                SetForegroundWindow(hwnd);
+                //Thread.Sleep(150);
+                PostMessage(hwnd, WM_KEYDOWN, (char)VK_RETURN, 0);
+                //Thread.Sleep(10);
+                PostMessage(hwnd, WM_KEYUP, (char)VK_RETURN, 0);
+                return;
+            }
+            try
+            {
+                //активизируем окно, которое имело фокус
+                SetForegroundWindow(hwnd);
+                Thread.Sleep(100);
+                //передаем ему текст посимвольно
+                foreach (char ch in message)
+                {
+                    PostMessage(hwnd, WM_CHAR, ch, 1);
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+        }
+    }
+    public class Account
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public int Status { get; set; }
+
+        public int PID { get; set; }
+
+        public override string ToString()
+        {
+            return Login;
+        }
+        public Account()
+        {
+
+        }
+        public Account(string login, string password, bool isStarted)
+        {
+            this.Login = Login;
+            this.Password = Password;
+        }
+
+        public Account(string login, string password)
+        {
+            this.Login = login;
+            this.Password = password;
+        }
+
+
+    }
+
+    public class ConfigObject
+    {
+        public string StartParams { get; set; }
+
+        public List<AccountsGroup> Groups { get; set; }
+
+        public ConfigObject()
+        {
+
+        }
+    }
+
+    public static class Config
+    {
+        private static ConfigObject config = new ConfigObject();
+
+        public static string DirectoryPath = "VenchassPanel"; //update's with initializing components
+
+        public static async void SaveAccountsDataAsync()
+        {
+            var json = System.Text.Json.JsonSerializer.Serialize(AccountManager.GetAccountsBase());
+            // полная перезапись файла 
+            using (StreamWriter writer = new StreamWriter(DirectoryPath + @"/Accounts.cfg", false))
+            {
+                await writer.WriteLineAsync(json);
+            }
+        }
+
+        public static async void SaveGroupsParams(List<AccountsGroup> groups)
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            config.Groups = groups;
+            var json = System.Text.Json.JsonSerializer.Serialize(config, options);
+            using (StreamWriter writer = new StreamWriter(DirectoryPath + @"/config.cfg", false))
+            {
+                await writer.WriteLineAsync(json);
+            }
+        }
+
+        public static void LoadAccountsData()
+        {
+            var json = "";
+            using (StreamReader reader = new StreamReader(DirectoryPath + @"/Accounts.cfg"))
+            {
+                json = reader.ReadToEnd();
+            }
+            try
+            {
+                var accountsList = System.Text.Json.JsonSerializer.Deserialize<List<Account>>(json);
+                foreach (var item in accountsList)
+                {
+                    AccountManager.AddAccount(item);
+                }
+            }
+            catch
+            {
+                File.WriteAllText(DirectoryPath + @"/Accounts.cfg", @"[]");
+            }
+            
+        }
+
+        public static async void SaveStartUpParams(string startupParams)
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            config.StartParams = startupParams;
+            var json = System.Text.Json.JsonSerializer.Serialize(config, options);
+            using (StreamWriter writer = new StreamWriter(DirectoryPath + @"/config.cfg", false))
+            {
+                await writer.WriteLineAsync(json);
+            }
+        }
+
+        public static ConfigObject LoadConfig()
+        {
+            try
+            {
+                var json = "";
+                using (StreamReader reader = new StreamReader(DirectoryPath + @"/config.cfg"))
+                {
+                    json = reader.ReadToEnd();
+                }
+                config = System.Text.Json.JsonSerializer.Deserialize<ConfigObject>(json);
+            }
+            catch
+            {
+                MessageBox.Show("не удалось загрузить конфиг");
+            }
+            return config;
+        }
+    }
+
+    public static class SteamGuard
+    {
+        private static bool IsInited = false;
+
+        private static Dictionary<string, string> SteamGuardDict = new Dictionary<string, string>();
+
+        private static string GetsharedSecret(string login)
+        {
+            if (SteamGuardDict.ContainsKey(login))
+                return SteamGuardDict[login];
+            return null;
+        }
+
+        public static bool HasGuard(string login)
+        {
+            if (!IsInited)
+                init();
+            return SteamGuardDict.ContainsKey(login);
+        }
+
+        public static string GetGuard(string login)
+        {
+            if (!IsInited)
+                init();
+            SteamGuardAccount account = new SteamGuardAccount();
+            account.SharedSecret = GetsharedSecret(login);
+            return account.GenerateSteamGuardCode();
+        }
+
+        private class SdaAccount
+        {
+            public string shared_secret { get; set; }
+            public string account_name { get; set; } //login
+        }
+
+        private static void init()
+        {
+            //init
+            DirectoryInfo dir = new DirectoryInfo(Config.DirectoryPath + @"/Steam Desktop Authenticator/maFiles");
+            if (!dir.Exists)
+                dir.Create();
+
+            foreach (FileInfo file in dir.GetFiles("*.maFile"))
+            {
+                var account = JsonConvert.DeserializeObject<SdaAccount>(File.ReadAllText(file.FullName));
+                SteamGuardDict.Add(account.account_name, account.shared_secret);
+            }
+
+
+            IsInited = true;
+        }
+    }
+}
